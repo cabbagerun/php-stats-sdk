a7576538c577afed9a9b320f0d9f5e0d52c5d07b
wenjian <huangwenjian@jianzhikeji.com>
2021-07-23 14:51:34 +0800

    fix

--- src/controller/Stats.php
+++ src/controller/Stats.php
@@ -5,9 +5,9 @@ namespace Jianzhi\Stats\controller;
 use Jianzhi\Stats\base\ControllerBase;
 use Jianzhi\Stats\service\ch\Test;
 
-class Qeury extends ControllerBase
+class Stats extends ControllerBase
 {
-    public function receive()
+    public function select()
     {
         $test = new Test();
         return $this->returnData(1000, 'ok', $test->selectTest());
