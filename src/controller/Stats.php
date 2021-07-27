e3839fefb2cc05248935179fa8a907996813862e
wenjian <huangwenjian@jianzhikeji.com>
2021-07-27 17:05:08 +0800

    fix

--- src/controller/Stats.php
+++ src/controller/Stats.php
@@ -9,10 +9,10 @@ class Stats extends ControllerBase
     public function select()
     {
         $params = $this->request->getParams();
-        $validate = new \Jianzhi\Stats\validate\DataStats();
-        if (!$validate->check($params)) {
-            return api_return(1001, $validate->getError());
-        }
+        // $validate = new \Jianzhi\Stats\validate\DataStats();
+        // if (!$validate->check($params)) {
+        //     return api_return(1001, $validate->getError());
+        // }
         return api_return(1000, 'ok', [$params]);
     }
 }
